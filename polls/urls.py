669447d8108c2f2393205061115d26a787c23d6a
岩佐琢 <iwasataku@iwasatakunoMacBook-Pro.local>
2022-01-30 20:19:16 +0900

    modify static

--- polls/urls.py
+++ polls/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+import settings
 
 
 urlpatterns = [
     path('', include('polls20220129tutorial.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
